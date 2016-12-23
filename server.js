5f65c0e333dd583408f07dcba881503a52b29a34
Anbus <s_anbus@rediffmail.com>
2016-12-23 15:53:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ var util = require("util");
 console.log("*************************************anbu start process env ");
 console.log(util.inspect(process.env, false, null));
 console.log("*************************************anbu end process env ");
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080; //process.env.MYNODEJS_SERVICE_PORT; //
+var server_port = process.env.PORT || 5000; //process.env.OPENSHIFT_NODEJS_PORT || 8080; //process.env.MYNODEJS_SERVICE_PORT; //
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; //process.env.MYNODEJS_SERVICE_HOST; // '172.30.162.2'; //
 //console.log("server port " + server_port + " server ip " + server_ip_address ) ;
-server.listen(8080, function () {
+server.listen(server_port, function () {
   //console.log( "Listening on " + server_ip_address + ", port " + server_port );
-  console.log("started listening .. anbu.. ");
+  console.log("started listening .. anbu.. port " + server_port);
 });
 /*server.listen("http://anbunodejs-learnter.44fs.preview.openshiftapps.com/", function () {
   console.log( "Listening on " + "http://anbunodejs-learnter.44fs.preview.openshiftapps.com/" );
