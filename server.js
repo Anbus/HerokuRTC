749684e255fe9761d061eccb8ddbfd8dc2a90e79
Anbus <s_anbus@rediffmail.com>
2016-12-23 10:59:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var respContent ="<HTML>Default page!</HTML>";
 var contentType = 'text/html';
 server.on("request", onRequest );
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT; // || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP; // || '127.0.0.1';
 console.log("server port " + server_port + " server ip " + server_ip_address ) ;
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
