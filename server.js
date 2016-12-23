dc4c36bc0ba47398a20f7e6927534fa73a865fc8
Anbus <s_anbus@rediffmail.com>
2016-12-23 13:28:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ console.log("*************************************anbu end process env ");
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080; //process.env.MYNODEJS_SERVICE_PORT; //
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; //process.env.MYNODEJS_SERVICE_HOST; // '172.30.162.2'; //
 console.log("server port " + server_port + " server ip " + server_ip_address ) ;
-/*server.listen(server_port, server_ip_address, function () {
+server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port );
-});*/
-server.listen("http://anbunodejs-learnter.44fs.preview.openshiftapps.com/", function () {
-  console.log( "Listening on " + "http://anbunodejs-learnter.44fs.preview.openshiftapps.com/" );
 });
+/*server.listen("http://anbunodejs-learnter.44fs.preview.openshiftapps.com/", function () {
+  console.log( "Listening on " + "http://anbunodejs-learnter.44fs.preview.openshiftapps.com/" );
+});*/
 
 function onRequest(req,resp){ 
 		console.log(req.url);
